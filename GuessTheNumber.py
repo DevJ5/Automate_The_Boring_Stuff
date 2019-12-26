db70ca0842412dca90244f44e0c67787aa7fdf9d
DevJ5 <idevelopjs@gmail.com>
2019-12-26 16:10:24 +0100

    Exception handling

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -6,21 +6,30 @@ minNumber = 1
 maxNumber = 20
 randomNumber = random.randint(minNumber,maxNumber)
 print("Well, {}, I am thinking of a number between {} and {}".format(name, minNumber, maxNumber))
-print(randomNumber)
+# print(randomNumber)
 # Ask the player to guess a maximum of 6 times
 for guessesTaken in range(1,7):
 	print("Take a guess")
-	guess = int(input())
-	if guess < randomNumber:
-		print("Thats too low")
-	elif guess > randomNumber:
-		print("Thats too high")
+	try:
+		guess = int(input())
+		if guess < randomNumber:
+			print("Thats too low")
+		elif guess > randomNumber:
+			print("Thats too high")
+		else:
+			break
+	except:
+		print("Thats not a valid number...")
+
+try:
+	# no block scope within loops
+	if guess == randomNumber:
+		print("Yay, correct! The number was %s." % randomNumber)
 	else:
-		break
+		print("You have guessed 6 times and didnt get it right. Better luck next time!")
+except:
+	print("You have entered an invalid number and it was your last try... Better luck next time!")
 
 
-# no block scope within loops
-if guess == randomNumber:
-	print("Yay, correct! The number was %s." % randomNumber)
-else:
-	print("You have guessed 6 times and didnt get it right. Better luck next time!")
+
+
