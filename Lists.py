a07df2443cc6675c953b21b8eca4ef902a57bf3c
DevJ5 <idevelopjs@gmail.com>
2019-12-26 19:08:52 +0100

    Lists

---
+++ Lists.py
@@ -0,0 +1,8 @@
+arr = ['cat', 'rat', 'dog']
+
+# Negative indexes count from the end, -1 is the last index.
+print("There is a %s." % arr[-1])
+
+# Ranges are done with a colon, from:to (but not include).
+print(arr[1:3])
+
--- Testfile.py
+++
@@ -1,5 +0,0 @@
-x = 10
-if x < 10:
-    print("less than 10")
-else:
-    print("not less than 10")
