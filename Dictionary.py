b073d3b86e6a6ec2c8451e582fe2eb26ef35b616
DevJ5 <idevelopjs@gmail.com>
2020-01-01 20:20:29 +0100

    Setdefault method

--- Dictionary.py
+++ Dictionary.py
@@ -14,7 +14,22 @@ for topping, price in pizzas.items():
 
 print('cheese' in pizzas) # True
 print('ham' not in pizzas) # True 
+
 # print(pizzas['ham']) # KeyError
+# Solution:
+if 'ham' in pizzas:
+    print(pizzas['ham'])
+# Alternative solution:
+print(pizzas.get('ham', "doesnt exist"))
+
+# There is also a setdefault method on dictionaries
+message = '''It was a bright cold day in April when I parked my car.'''
+count = {}
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] +=1
+
+print(count)
 
 print(pizzas.values())  # Returns a listlike datatype (dict_values)
 list(pizzas.values())   # Returns list of values
