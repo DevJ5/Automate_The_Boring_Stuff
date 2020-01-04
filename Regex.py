8275a2d622ddb21bb88351a7cf5aac1236c4eb92
DevJ5 <idevelopjs@gmail.com>
2020-01-04 20:21:44 +0100

    RegEx

--- Regex.py
+++ Regex.py
@@ -8,6 +8,15 @@ matchObject = phonenumberRegEx.search(phoneNumber)
 print(matchObject.group())
 
 # Grouping syntax
-phonenumberRegEx = re.compile(r"(\d\d\d)")
+phonenumberRegEx = re.compile(r"(\d{3})-(\d{3})-(\d{4})")
 matchObject2 = phonenumberRegEx.search(phoneNumber) # Notice the use of the raw string!
 print(matchObject2.group(2))
+
+batRegEx = re.compile(r"Bat(man|mobile|copter)", re.IGNORECASE)
+print(batRegEx.findall("Batman lost a batmobile"))
+matchObject3 = batRegEx.search("Batman lost a batmobile")
+# Can return None as object
+if matchObject3 != None:
+    print(matchObject3.group(1))
+
+
